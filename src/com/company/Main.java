0e81ac17f2c2e2a67618612c3511e7c959cd3153
Janudanie <alex.janudanie@gmail.com>
2020-02-13 23:49:04 +0100

    Added the name text files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class Main {
     public static void main(String[] args)  {
-    String pathToFirstNameFile =  "C:\\temp\\fornavnesort.txt";
-    String pathToLastNameFile = "C:\\temp\\efternavnesort.txt";
+    String pathToFirstNameFile = "firstnames.txt";
+    String pathToLastNameFile = "lastnames.txt";
     getNames test = new getNames(pathToFirstNameFile,pathToLastNameFile);
     for(int i = 0 ; i<10 ;i++) System.out.println(test.getRandomFirstName()+ " " + test.getRandomLastName());
     }
